d4a1ee89d53e371765c07b4395cfd2224f3da5e9
Alfred <alfredkangjr@gmail.com>
2023-12-02 00:00:56 +0800

    feat: mvp

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
-
-const navBar = document.getElementsByClassName("navBar")[0]
-const navMenu = navBar.getElementsByClassName("navMenu")[0]
-const navIcon = navMenu.getElementsByClassName("navIcon")
-// ??? what u mean what look at index.html, navIcon DOESNT EXiST check tele
-
-// const navMenu = document.getElementsByClassName(".navMenu");
-// const navItem = document.getElementsByClassName(".navItem");
-// const navIcon= document.getElementsByClassName(".navIcon");
-
-function hamburgerMenu() {
-  if (navMenu.classList.contains("showNavMenu")) {
-    navMenu.classList.remove("showNavMenu");
-  } else {
-    navMenu.classList.add("showNavMenu");
-
+document.addEventListener("DOMContentLoaded", () => {
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
   }
-  navIcon.addEventListener("click", "toggleMenu");
-}
+  
+  const navBar = document.getElementsByClassName("navBar")[0]
+  const navMenu = document.getElementsByClassName("navMenu")[0]
+  const navIcon = document.getElementsByClassName("navIcon")[0]
+
+  navIcon.addEventListener("pointerdown", () => {
+    if (navIcon.style.color === "#ebd6b6") {
+      navIcon.style.color = "#DB5A42"
+    } else {
+      navIcon.style.color = "#ebd6b6"
+    }
+
+    if (navMenu.classList.contains("showNavMenu")) {
+      navMenu.classList.remove("showNavMenu");
+    } else {
+      navMenu.classList.add("showNavMenu");
+    }
+  })
+})
